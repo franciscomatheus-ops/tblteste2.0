89c57cca55f2ecb263d67ac5b78ff96f76be87ad
MatheusNewDev <128175085+franciscomatheus-ops@users.noreply.github.com>
2025-03-05 10:37:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function LinesMenu() {
             <span id=${Line.LineID} onclick=LineClick(this.id) ${Configs.Menu.LineChose == Line.LineID ? "class=LineActive" : ''}>${Line.NameLine}</span>
         `;
     });
+    CriacaoTwoTables();
 }
 
 function LineClick(line) {
@@ -26,16 +27,20 @@ function LineClick(line) {
 function CriacaoTwoTables() {
     let Configs = JSON.parse(localStorage.getItem('Configs'));
     let mainDiv = document.getElementById('TwoTables');
+    mainDiv.innerHTML = '';
 
     let PartidasPontos = "";
     let PartidasAbates = "";
-    for (let x = 1; x <= Configs.Menu.Quedas; x++){
-        PartidasPontos += `<tr>
-            <td>#${x}</td>
-            <td><input type=number id=q${x}></td>
-            <td><input type=number id=a${x}></td>
-            <td></td>
-        </tr>`;
+
+
+    for (let x = 1; x <= Configs.Menu.Quedas; x++) {
+        PartidasPontos += `
+            <tr>
+                <td>#${x}</td>
+                <td><input type=number id=q${x} oninput="ValueUpdate(this.id, this.value, p${x})"></td>
+                <td><input type=number id=a${x} oninput="ValueUpdate(this.id, this.value, p${x})"></td>
+                <td id=p${x}></td>
+            </tr>`;
         PartidasAbates += `
             <tr>
                 <td>#${x}</td>
@@ -43,15 +48,14 @@ function CriacaoTwoTables() {
                 <td><input type=number></td>
                 <td><input type=number></td>
                 <td><input type=number></td>
-            </tr>
-        `;
+            </tr>`;
     }
-    
+
     mainDiv.innerHTML += `
         <table id=table1 class=TableChose>
             <tr>
                 <td>Line</td>
-                <td colspan=3><input type=text oninput=RenameLine(this.value)></td>
+                <td colspan=3><input type=text oninput=RenameLine(this.value) id=NameLine></td>
                 <td rowspan=${Configs.Menu.Quedas + 3} class=btnPlayer onclick="SwitchTbl(2)">Players</td>
             </tr>
             <tr>
@@ -63,7 +67,7 @@ function CriacaoTwoTables() {
             ${PartidasPontos}
             <tr>
                 <td colspan=3>Pontuação</td>
-                <td>0</td>
+                <td id=res>0</td>
             </tr>
         </table>
 
@@ -82,7 +86,7 @@ function CriacaoTwoTables() {
             ${PartidasAbates}
         </table>                                            
     `;
-    
+    validarTable();
 }
 
 function SwitchTbl(key) {
@@ -91,7 +95,63 @@ function SwitchTbl(key) {
         document.getElementById('table2').classList = 'TableChose';
     }
     else {
-        document.getElementById('table2').classList = 'TableNoChose';    
+        document.getElementById('table2').classList = 'TableNoChose';
         document.getElementById('table1').classList = 'TableChose';
     }
+}
+
+function RenameLine(inp) {
+    let Configs = JSON.parse(localStorage.getItem('Configs'));
+    Configs.Lines.forEach(Line => {
+        if (Line.LineID == Configs.Menu.LineChose) {
+            document.getElementById(Line.LineID).innerText = inp;
+            Line.NameLine = inp;
+        }
+        // console.log(Configs);
+
+    })
+    SendArray(Configs);
+}
+
+
+
+function SendArray(Array) {
+    localStorage.setItem('Configs', JSON.stringify(Array));
+}
+
+function validarTable() {
+    let Configs = JSON.parse(localStorage.getItem('Configs'))
+    Configs.Lines.forEach(L => {
+        if (L.LineID == Configs.Menu.LineChose) {
+            document.getElementById(`NameLine`).value = L.NameLine.toLowerCase().includes('line') ? '' : L.NameLine;
+            for (let x = 1; x <= Configs.Menu.Quedas; x++) {
+                document.getElementById(`a${x}`).value = L[`a${x}`] == 0 ? "" : L[`a${x}`];
+                document.getElementById(`q${x}`).value = L[`q${x}`] == 0 ? "" : L[`q${x}`];
+                document.getElementById(`p${x}`).innerText = L[`p${x}`] == 0 ? "" : L[`p${x}`];
+            }
+            document.getElementById('res').innerText = L.res;
+        }
+    })
+}
+
+function ValueUpdate(id, value, ponto) {
+    let Configs = JSON.parse(localStorage.getItem('Configs'));
+    Configs.Lines.forEach(l => {
+        if (l.LineID == Configs.Menu.LineChose) {
+            l[id] = value == '' ? 0 : parseInt(value)
+            let r = 0;
+            for (let x = 1; x <= Configs.Menu.Quedas; x++) {
+                let a = isNaN(l[`a${x}`]) ? 0 : l[`a${x}`];
+                console.log(a);
+
+                l[`p${x}`] = l[`q${x}`] == 1 ? 12 + l[`a${x}`] : l[`q${x}`] >= 2 && l[`q${x}`] <= 10 ? (11 - l[`q${x}`]) + l[`a${x}`] : l[`a${x}`];
+                r += l[`p${x}`];
+            }
+            l.res = r;
+            ponto.innerText = l[ponto.id];
+            document.getElementById('res').innerText = r;
+        }
+    })
+
+    SendArray(Configs);
 }
--- config.js
+++ config.js
@@ -2,14 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!localStorage.getItem("Configs")) {
     let Configs = {
       Menu: { Quedas: 5, LineChose:"Line_01", OldLine:""},
-      Lines: []
+      Lines: [],
+      Player: []
     };
     for (let x = 1; x <= 12; x++){
       let id = x < 10 ? `0${x}` : x;
       Configs.Lines.push({
-        NameLine: `Line ${id}`, LineID: `Line_${id}`, a1: 0, p1: 0, p: 0
+        NameLine: `Line ${id}`, LineID: `Line_${id}`, a1: 0, q1: 0, p1:0, res: 0
       })
     }
+    Configs.Lines.forEach(Lines => {
+      for (let x = 1; x <= Configs.Menu.Quedas; x++) {
+        Lines[`a${x}`] = Lines.hasOwnProperty(`a${x}`) ? Lines[`a${x}`] : 0;
+        Lines[`q${x}`] = Lines.hasOwnProperty(`q${x}`) ? Lines[`q${x}`] : 0;
+        Lines[`p${x}`] = Lines.hasOwnProperty(`p${x}`) ? Lines[`p${x}`] : 0;
+      }
+    })
     localStorage.setItem("Configs", JSON.stringify(Configs))
     window.location.reload();
   }
